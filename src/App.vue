f81f1d2473c0d169972d24a3117bd39abc47cc74
Higashimoto Kanami <summerwave5959@gmail.com>
2022-06-26 11:41:09 +0900

    [Start] vue.js cli使用

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <LikeHeader></LikeHeader>
+    <LikeNumber></LikeNumber>
+  </div>
+</template>
+
+<script>
+import LikeHeader from "./components/LikeHeader.vue";
+
+export default {
+  components: {
+    LikeHeader
+  }
+};
+</script>
+<style>
+</style>
+
---
+++ src/components/LikeNumber.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    <p>{{number}}</p>
+    <button @click="increment"></button>
+</div>
+</template>
+<script>
+exports default {
+    data() {
+        return {
+            number: 5
+        };
+    }
+}
+</script>
